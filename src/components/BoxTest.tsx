111c0ced8cadefc16bf5307338e6886a3e3965ca
Nicole Garcia <lpurgitoryl@gmail.com>
2023-11-18 14:56:58 -0800

    disable lint warning

--- src/components/BoxTest.tsx
+++ src/components/BoxTest.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import * as THREE from "three";
 import { useRef, useState } from "react";
 import { useFrame, ThreeElements } from "@react-three/fiber";
